28e9e7ec99d8d0fbea66b8c1f66fb4ebea1dc1f4
Udit Kumar Chatterjee <quantumudit@gmail.com>
2024-08-08 21:56:28 +0530

    added logger and exception in the code

--- src/components/data_processor.py
+++ src/components/data_processor.py
@@ -7,9 +7,8 @@ from os.path import dirname, normpath
 import pandas as pd
 
 from src.constants import CONFIGS
-
-# from src.exception import CustomException
-# from src.logger import logger
+from src.exception import CustomException
+from src.logger import logger
 from src.utils.basic_utils import (
     create_directories,
     get_lat_long,
@@ -51,64 +50,83 @@ class DataProcessor:
             return row["postcode"]
 
     def data_transformation(self):
-        # import the dataset
-        df = pd.read_csv(self.scraped_data_path)
+        try:
+            # import the dataset
+            df = pd.read_csv(self.scraped_data_path)
+            logger.info("Dataset imported")
 
-        # Drop duplicate rows
-        df_unq = df.drop(columns="scrape_ts").drop_duplicates()
-        df = df.loc[df_unq.index]
+            # Drop duplicate rows
+            df_unq = df.drop(columns="scrape_ts").drop_duplicates()
+            df = df.loc[df_unq.index]
+            logger.info("Duplicate entries dropped")
 
-        # Populate latitude and longitude, if absent
-        df["latitude"], df["longitude"] = zip(*df.apply(self.apply_lat_long_fn, axis=1))
+            # Populate latitude and longitude, if absent
+            df["latitude"], df["longitude"] = zip(
+                *df.apply(self.apply_lat_long_fn, axis=1)
+            )
+            logger.info("Latitude and Longitude populated, if absent")
 
-        # Extract state and postcode from address
-        df["state"] = df["address"].str.extract(
-            r".+?((?:[A-Z]{2,3}|Victoria|Vic|Western Australia))", expand=False
-        )
-        df["postcode"] = df["address"].str.extract(r".* (\d{4})", expand=False)
+            # Extract state and postcode from address
+            df["state"] = df["address"].str.extract(
+                r".+?((?:[A-Z]{2,3}|Victoria|Vic|Western Australia))", expand=False
+            )
+            df["postcode"] = df["address"].str.extract(r".* (\d{4})", expand=False)
+            logger.info("State and postcode extracted from the address")
 
-        # Handle inconsistencies in "state" column
-        df["state"] = (
-            df["state"]
-            .str.replace(r"Vic(?:toria)?", "VIC", regex=True)
-            .str.replace("Western Australia", "WA")
-        )
+            # Handle inconsistencies in "state" column
+            df["state"] = (
+                df["state"]
+                .str.replace(r"Vic(?:toria)?", "VIC", regex=True)
+                .str.replace("Western Australia", "WA")
+            )
+            logger.info("Handled in the inconsistencies in the state names")
 
-        # Populate postcode, if absent
-        df["postcode"] = df.apply(self.apply_postcode_function, axis=1)
+            # Populate postcode, if absent
+            df["postcode"] = df.apply(self.apply_postcode_function, axis=1)
+            logger.info("Postcode populated, if absent")
 
-        # Add an index column
-        custom_index_col = pd.RangeIndex(
-            start=1000, stop=1000 + len(df), step=1, name="id"
-        )
-        df.index = custom_index_col
-        df.index = "SVC" + df.index.astype("string")
-        df = df.reset_index()
+            # Add an index column
+            custom_index_col = pd.RangeIndex(
+                start=1000, stop=1000 + len(df), step=1, name="id"
+            )
+            df.index = custom_index_col
+            df.index = "SVC" + df.index.astype("string")
+            df = df.reset_index()
+            logger.info("Added a unique row ID for each distinct row")
 
-        # Unpivot "services_offered" column and strip contents
-        df["services_offered"] = df["services_offered"].str.split(",")
-        df_exploded = df.explode("services_offered")
-        df_exploded["services_offered"] = df_exploded["services_offered"].str.strip()
+            # Unpivot "services_offered" column and strip contents
+            df["services_offered"] = df["services_offered"].str.split(",")
+            df_exploded = df.explode("services_offered")
+            df_exploded["services_offered"] = df_exploded[
+                "services_offered"
+            ].str.strip()
+            logger.info("Unpivot service offered column")
 
-        # Use correct datatype for numeric columns
-        df_exploded["latitude"] = pd.to_numeric(
-            df_exploded["latitude"], errors="coerce"
-        )
-        df_exploded["longitude"] = pd.to_numeric(
-            df_exploded["longitude"], errors="coerce"
-        )
-        df_exploded["scrape_ts"] = pd.to_datetime(df_exploded["scrape_ts"])
+            # Use correct datatype for numeric columns
+            df_exploded["latitude"] = pd.to_numeric(
+                df_exploded["latitude"], errors="coerce"
+            )
+            df_exploded["longitude"] = pd.to_numeric(
+                df_exploded["longitude"], errors="coerce"
+            )
+            df_exploded["scrape_ts"] = pd.to_datetime(df_exploded["scrape_ts"])
+            logger.info("Provided proper datatype to columns")
 
-        # Fixing issue in Longitude
-        df_exploded["longitude"] = df_exploded["longitude"].apply(
-            lambda x: x + 100 if x < 100 else x
-        )
+            # Fixing issue in Longitude
+            df_exploded["longitude"] = df_exploded["longitude"].apply(
+                lambda x: x + 100 if x < 100 else x
+            )
+            logger.info("Fixing any source issues in the longitude column")
 
-        # Rearrange columns
+            # Rearrange columns
 
-        # create save directory if not exists
-        create_directories([dirname(self.processed_data_path)])
+            # create save directory if not exists
+            create_directories([dirname(self.processed_data_path)])
 
-        # Export transformed data
-        df_exploded.to_csv(self.processed_data_path, index=False)
-        return None
+            # Export transformed data
+            df_exploded.to_csv(self.processed_data_path, index=False)
+            logger.info("Transformed data saved at: %s", self.processed_data_path)
+            return None
+        except Exception as e:
+            logger.error(CustomException(e))
+            raise CustomException(e) from e
